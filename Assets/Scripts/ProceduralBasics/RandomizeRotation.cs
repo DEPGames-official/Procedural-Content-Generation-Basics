4c6a021883086274bb4af5310c1d8f3731418897
DEPGames-official <dreyespoppergames@gmail.com>
2023-09-05 11:44:36 +0200

    Uploading Old Project From Aug 6 2020

---
+++ Assets/Scripts/ProceduralBasics/RandomizeRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeRotation : MonoBehaviour
+{
+    public Vector3 randomRotationRange;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomizeByAxis(randomRotationRange);
+        //RandomizeMyRotation();    
+    }
+
+    void RandomizeMyRotation()
+    {
+        transform.rotation = Random.rotation;
+    }
+
+    public void RandomizeByAxis(Vector3 randomRotationConstraints)
+    {
+        Quaternion randomConstrainedRotation = Quaternion.Euler(transform.rotation.eulerAngles.x + Random.Range(-randomRotationConstraints.x, randomRotationConstraints.x),
+            transform.rotation.eulerAngles.y + Random.Range(-randomRotationConstraints.y, randomRotationConstraints.y),
+            transform.rotation.eulerAngles.z + Random.Range(-randomRotationConstraints.z, randomRotationConstraints.z));
+
+        this.transform.rotation = randomConstrainedRotation;
+    }
+    
+}
